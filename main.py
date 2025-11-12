aaded4467fdc4dae61f83558628e0cd9228c0772
Gabriel Eric <gabriel.eric@ufrpe.br>
2025-11-11 22:48:14 -0300

    ✨ feat: New templates

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from db_config import app
 import models.models
+from flask import Blueprint
+from routes.login import login_bp
+from routes.home import home_bp
 
-@app.route('/')
-def init():
-    return "Hello World"
+app.register_blueprint(login_bp)
+app.register_blueprint(home_bp, url_prefix='/home')
 
 if __name__ == "__main__":
     print("Iniciando o servidor Flask...")
